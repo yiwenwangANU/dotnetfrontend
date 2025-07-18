5f1f2ca7e9571a02c2132c191e444fbcfefeaa94
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-18 12:10:55 +1000

    update login page css to match the style of register page

--- src/app/identity/account/login/page.tsx
+++ src/app/identity/account/login/page.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/Buttom";
 import { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import useLogin from "@/hooks/useLogin";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
 type Inputs = {
   email: string;
   password: string;
@@ -56,13 +57,25 @@ const Login = () => {
           {...register("email", { required: "Please enter a email." })}
         />
         {errors.password && <span>{errors.password.message}</span>}
-        <input
-          type={showPassword ? "text" : "password"}
-          placeholder="Password"
-          className="border rounded border-gray-200 p-4 text-xl w-100"
-          {...register("password", { required: "Please enter your password." })}
-        />
+        <div className="flex flex-row gap-2 items-center">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            className="border rounded border-gray-200 p-4 text-xl w-100"
+            {...register("password", {
+              required: "Please enter your password.",
+            })}
+          />
+          <span onClick={togglePassword}>
+            {showPassword ? (
+              <EyeSlashIcon className="w-8 h-8 text-gray-400" />
+            ) : (
+              <EyeIcon className="w-8 h-8 text-gray-400" />
+            )}
+          </span>
+        </div>
         {errors.password && <span>{errors.password.message}</span>}
+
         <div className="flex flex-row gap-3">
           <input type="checkbox" id="rememberMe" {...register("rememberMe")} />
           <label htmlFor="rememberMe">Remember me?</label>
