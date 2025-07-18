2a03699f1326395b0f800145b2487d30d1895532
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-18 11:19:47 +1000

    update ui for better ux

--- src/app/identity/account/register/page.tsx
+++ src/app/identity/account/register/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 import Button from "@/components/Buttom";
+import {
+  EyeDropperIcon,
+  EyeIcon,
+  EyeSlashIcon,
+} from "@heroicons/react/16/solid";
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
@@ -16,7 +22,7 @@ const Register = () => {
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-
+  const password = watch("password");
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
     // mutate({
@@ -24,17 +30,17 @@ const Register = () => {
     //   password: data.password,
     // });
   };
-  const password = watch("password");
-
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => setShowPassword((prev) => !prev);
   return (
     <div className="flex flex-col gap-2 justify-center items-center">
-      <div className="text-4xl">Login</div>
-      <div className="text-2xl ">Use a local account to log in</div>
+      <div className="text-4xl">Register</div>
+      <div className="text-2xl ">Create a new Account</div>
       <hr className="border-t-2 border-gray-600 mb-4" />
       <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <input
           placeholder="Email"
-          className="border rounded border-gray-200 p-4 text-xl"
+          className="border rounded border-gray-200 p-4 text-xl w-100"
           {...register("email", {
             required: "Please enter a email.",
             pattern: {
@@ -46,27 +52,37 @@ const Register = () => {
         {errors.email && (
           <span className="text-red-600">{errors.email.message}</span>
         )}
-        <input
-          type="password"
-          placeholder="Password"
-          className="border rounded border-gray-200 p-4 text-xl"
-          {...register("password", {
-            required: "Please enter the password.",
-            minLength: {
-              value: 6,
-              message: "A valid password must between 6 to 20 characters.",
-            },
-            maxLength: {
-              value: 20,
-              message: "A valid password must between 6 to 20 characters.",
-            },
-            pattern: {
-              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/,
-              message:
-                "A valid password must include uppercase, lowercase, number, and symbol(e.g., !@#$%)",
-            },
-          })}
-        />
+        <div className="flex flex-row items-center gap-2">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            className="border rounded border-gray-200 p-4 text-xl w-100"
+            {...register("password", {
+              required: "Please enter the password.",
+              minLength: {
+                value: 6,
+                message: "A valid password must between 6 to 20 characters.",
+              },
+              maxLength: {
+                value: 20,
+                message: "A valid password must between 6 to 20 characters.",
+              },
+              pattern: {
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/,
+                message:
+                  "A valid password must include uppercase, lowercase, number, and symbol(e.g., !@#$%)",
+              },
+            })}
+          />
+          <span onClick={togglePassword}>
+            {showPassword ? (
+              <EyeSlashIcon className="w-8 h-8 text-gray-400" />
+            ) : (
+              <EyeIcon className="w-8 h-8 text-gray-400" />
+            )}
+          </span>
+        </div>
+
         {/* errors will return when field validation fails  */}
         {errors.password && (
           <span className="text-red-600">{errors.password.message}</span>
@@ -75,7 +91,7 @@ const Register = () => {
           <input
             type="password"
             placeholder="Confirm Password"
-            className="border rounded border-gray-200 p-4 text-xl"
+            className="border rounded border-gray-200 p-4 text-xl w-100"
             {...register("re_password", {
               required: "Please confirm your password.",
               validate: (value) =>
