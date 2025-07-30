8fc0d16738e4b90ddd6d27c895d15301674e7930
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-30 11:36:04 +1000

    so minor change and invalidate get profile after login

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,15 +1,17 @@
 "use client";
 import { LoginResponse, loginUser } from "@/api/apiAuth";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 const useLogin = () => {
   const router = useRouter();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: loginUser,
     onSuccess: (res: LoginResponse) => {
       toast.success(`Welcome back, ${res.email}!`);
+      queryClient.invalidateQueries({ queryKey: ["getProfile"] });
       router.push("/");
     },
   });
